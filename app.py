e16ea173926315e023887fef5f99984eba1b7367
Brian Zeng <brianzeng@Brians-MacBook-Air.local>
2026-07-08 22:50:46 -0700

    Fix Render port binding

--- app.py
+++ app.py
@@ -876,7 +876,7 @@ def create_app() -> Flask:
 
 if __name__ == "__main__":
     create_app().run(
-        host = os.environ.get("HOST", "127.0.0.1"),
+        host = os.environ.get("HOST", "0.0.0.0"),
         port = int(os.environ.get("PORT", "5000")),
         debug = os.environ.get("FLASK_DEBUG") == "1",
     )
